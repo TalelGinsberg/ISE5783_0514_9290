4da37681b5ab2cab50671638bb17c5cb76915476
TalelGinsberg <taleltov@g.jct.ac.il>
2023-04-20 12:36:56 +0300

    mid STAGE3

--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -32,8 +32,23 @@ public class Triangle extends Polygon{
     public List<Point> findIntersections(Ray ray) {
         Plane plane=new Plane(vertices.get(0),vertices.get(1).subtract(vertices.get(2)));
         List <Point>points =plane.findIntersections(ray);
-        if (points.equals(null))
+        if (points==null)
                 return null;
-        if
+        Vector V=vertices.get(1).subtract(vertices.get(0));
+        Vector U= vertices.get(2).subtract(vertices.get(0));
+        double areaTriangle=0.5/V.dotProduct(U);
+        Vector v1=vertices.get(1).subtract(vertices.get(0));
+        Vector u1= points.get(0).subtract(vertices.get(0));
+        double areaTriangle1=0.5/V.dotProduct(U);
+        Vector v2=vertices.get(1).subtract(points.get(0));
+        Vector u2= vertices.get(2).subtract(points.get(0));
+        double areaTriangle2=0.5/V.dotProduct(U);
+        Vector v3=points.get(0).subtract(vertices.get(0));
+        Vector u3= vertices.get(2).subtract(vertices.get(0));
+        double areaTriangle3=0.5/V.dotProduct(U);
+        if (areaTriangle3+areaTriangle1+areaTriangle2==areaTriangle)
+            return points;
+        return null;
+
     }
 }
