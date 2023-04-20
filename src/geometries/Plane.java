e77193a562afed25e9af213738fe759c488f48af
noharel <noharel@g.jct.ac.il>
2023-04-20 14:55:23 +0300

    sync

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -75,7 +75,7 @@ public class Plane implements Geometry{
         if (isZero(nv))
             //throw new IllegalArgumentException("The scalar product of the normal with the ray vector is zero");
            return null;
-        double nQminusP0 = normal.dotProduct(ray.getP0().subtract(q0));
+        double nQminusP0 = normal.dotProduct(q0.subtract(ray.getP0()));
         double t = alignZero(nQminusP0 / nv);
         //if (t>0)
             return List.of(ray.getP0().add(ray.getDrr().scale(t)));
