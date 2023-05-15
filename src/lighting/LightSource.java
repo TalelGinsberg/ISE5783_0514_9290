4872cb61a213126827ffb3a0f38e61751221d244
noharel <noharel@g.jct.ac.il>
2023-05-16 02:24:22 +0300

    final doc stage 6

--- src/lighting/LightSource.java
+++ src/lighting/LightSource.java
@@ -4,7 +4,27 @@ import primitives.Color;
 import primitives.Point;
 import primitives.Vector;
 
+
+/**
+ * An interface representing a light source in a 3D scene.
+ *
+ * @author Noa Harel and Talel Ginsberg
+ */
 public interface LightSource {
+
+    /**
+     * Returns the intensity of the light at the specified point.
+     *
+     * @param p The point at which to evaluate the intensity of the light.
+     * @return The intensity of the light at the specified point.
+     */
     public Color getIntensity(Point p);
+
+    /**
+     * Returns the direction of the light at the specified point.
+     *
+     * @param p The point at which to evaluate the direction of the light.
+     * @return The direction of the light at the specified point.
+     */
     public Vector getL(Point p);
 }
--- src/lighting/SpotLight.java
+++ src/lighting/SpotLight.java
@@ -6,14 +6,39 @@ import primitives.Vector;
 
 import static java.lang.Math.max;
 
+
+/**
+ * A type of light source that emits light in a specific direction from a
+ * point source and attenuates with distance.
+ *
+ * @author Noa Harel and Talel Ginsberg
+ */
 public class SpotLight extends PointLight{
 
+    //----------------------------fields--------------------------
+
+    /**
+     * The direction of the spotlight.
+     */
+    private Vector direction;
+
+
+    //-----------------------------constructor-------------------------
+
+    /**
+     * Constructs a new SpotLight object with the specified intensity, position and direction.
+     *
+     * @param intensity The intensity of the spot light.
+     * @param position The position of the spot light.
+     * @param direction The direction of the spotlight.
+     */
     public SpotLight(Color intensity, Point position, Vector direction) {
         super(intensity, position);
         this.direction = direction.normalize();
     }
 
-    private Vector direction;
+
+    //---------------------------override functions-------------------------
 
     @Override
     public Color getIntensity(Point p) {
